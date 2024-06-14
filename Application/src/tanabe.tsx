1c417de12a06c5e78c541a5bd9ba5cd6002efc3f
tana4242 <ryota_tanabe-7020@jouhou-giken.info>
2024-06-14 11:13:35 +0900

    tanabe.tsxのボタンのデザイン変更

--- Application/src/tanabe.tsx
+++ Application/src/tanabe.tsx
@@ -18,11 +18,11 @@ const CalTanabe:React.FC<Cal> = ({inputA,inputB}) => {
   return (
     <div>
       <Box display="flex" alignItems="center" gap={1}>
-        <Button onClick={handleCal}>×</Button>
+        <Button variant="contained"onClick={handleCal}>×</Button>
         <TextField value={num} disabled/>
       </Box>
       <Box display="flex" alignItems="center" gap={1} marginTop={1}>
-        <Button onClick={handleCal1}>÷</Button>
+        <Button variant="contained"onClick={handleCal1}>÷</Button>
         <TextField value={num1} disabled/>
       </Box>
     </div>

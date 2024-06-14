c7484136997d387626129c5f7ac3dc09f4f0561c
NT <bo1122f@gmail.com>
2024-06-14 11:54:14 +0900

    更新後髙澤

---
+++ Application/src/takazawa.tsx
@@ -0,0 +1,25 @@
+import { TextField, Button, Box } from "@mui/material"
+import React, { useState } from "react"
+
+type Cal = {
+    inputA:number,
+    inputB:number
+}
+
+const CalTakazawa:React.FC<Cal> = ({inputA,inputB}) => {
+const [num, setNum] = useState<number>(0);
+
+const hundleMinusClick = () => {
+    setNum(inputA - inputB);
+};
+
+return(
+    <div>
+        <Box display="flex" alignItems="center" gap={1}>
+        <Button onClick={hundleMinusClick}>-</Button>
+        <TextField value={num} disabled/>
+        </Box>
+        </div>
+      );
+    };
+export default CalTakazawa;

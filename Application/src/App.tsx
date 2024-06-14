f66689e98bb2757103ae75f546b081f9d8f8b178
NT <bo1122f@gmail.com>
2024-06-14 15:35:10 +0900

    15:35 更新App

--- Application/src/App.tsx
+++ Application/src/App.tsx
@@ -1,47 +1,57 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { TextField, Button, Grid, Box } from '@mui/material';
 import CalTanabe from './tanabe.tsx';
 import CalTakazawa from './takazawa.tsx';
 import { Kitami } from './kitami.tsx';
-
 
 function App() {
   const [inputA, setInputA] = useState<number>(0);
   const [inputB, setInputB] = useState<number>(0);
 
   return (
-    <><Grid container spacing={2}>
-      <Grid item lg={6}>
-      <Box    
-        display="flex"
-        flexDirection="column"
-        alignItems="flex-start"
-        justifyContent="flex-start"
-      >
-        <TextField
-          type="number"
-          placeholder='数値Aを入力'
-          value={inputA || ""}
-          onChange={(e) => setInputA(Number(e.target.value))}
-          margin="normal"
-          style={{ color: "#000000", backgroundColor: "#00ffff" }} 
-        />
-        <TextField
-          type="number"
-          placeholder='数値Bを入力'
-          value={inputB || ""}
-          onChange={(e) => setInputB(Number(e.target.value))}
-          margin="normal"
-          style={{ color: "#000000", backgroundColor: "#00ffff" }} 
-        />
-      </Box>
+    <Box 
+      display="flex" 
+      flexDirection="column" 
+      alignItems="center" 
+      justifyContent="center" 
+      height="100vh"
+    >
+      <Grid container spacing={50}>
+        <Grid item xl={6}     alignItems="center"
+            justifyContent="center">
+          <Box
+          marginLeft={35}
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <TextField
+              type="number"
+              placeholder='数値Aを入力'
+              value={inputA || ""}
+              onChange={(e) => setInputA(Number(e.target.value))}
+              margin="normal"
+              style={{ color: "#000000", backgroundColor: "#00ffff" }} 
+            />
+            <TextField
+              type="number"
+              placeholder='数値Bを入力'
+              value={inputB || ""}
+              onChange={(e) => setInputB(Number(e.target.value))}
+              margin="normal"
+              style={{ color: "#000000", backgroundColor: "#00ffff" }} 
+            />
+          </Box>
+        </Grid>
+        <Grid item xl={6}   alignItems="center"
+            justifyContent="center">
+          <Kitami inputNum1={inputA} inputNum2={inputB} />
+          <CalTakazawa inputA={inputA} inputB={inputB} />
+          <CalTanabe inputA={inputA} inputB={inputB} />
+        </Grid>
       </Grid>
-      <Grid item lg={6}>
-      <Kitami inputNum1={inputA} inputNum2={inputB} />
-      <CalTakazawa inputA={inputA} inputB={inputB}  />
-      <CalTanabe inputA={inputA} inputB={inputB} />
-      </Grid></Grid>
-    </>  
+    </Box>  
   );
 }
 

be66e96207fb608b3a04ec02caea448e812f6161
tana4242 <ryota_tanabe-7020@jouhou-giken.info>
2024-06-14 11:26:05 +0900

    tanabe.tsxのデザイン修正

--- Application/src/tanabe.tsx
+++ Application/src/tanabe.tsx
@@ -1,11 +1,11 @@
 import { Button, TextField, Box } from "@mui/material";
 import React, { useState } from "react";
 
- type Cal={
-    inputA:number,
-    inputB:number
-}
-const CalTanabe:React.FC<Cal> = ({inputA,inputB}) => {
+type Cal = {
+  inputA: number;
+  inputB: number;
+};
+const CalTanabe: React.FC<Cal> = ({ inputA, inputB }) => {
   const [num, setNum] = useState<number>(0);
   const [num1, setNum1] = useState<number>(0);
   const handleCal = () => {
@@ -17,13 +17,17 @@ const CalTanabe:React.FC<Cal> = ({inputA,inputB}) => {
 
   return (
     <div>
-      <Box display="flex" alignItems="center" gap={1}>
-        <Button variant="contained"onClick={handleCal}>×</Button>
-        <TextField value={num} disabled/>
+      <Box display="flex" alignItems="center" gap={3} marginTop={3}>
+        <Button variant="contained" onClick={handleCal}>
+          ×
+        </Button>
+        <TextField label="掛け算の結果が表示されます" value={num} disabled />
       </Box>
-      <Box display="flex" alignItems="center" gap={1} marginTop={1}>
-        <Button variant="contained"onClick={handleCal1}>÷</Button>
-        <TextField value={num1} disabled/>
+      <Box display="flex" alignItems="center" gap={3} marginTop={3}>
+        <Button variant="contained" onClick={handleCal1}>
+          ÷
+        </Button>
+        <TextField label="割り算の結果が表示されます" value={num1} disabled />
       </Box>
     </div>
   );

48f8d018e92541320ff7b9ebc7d2f43b78a8314a
NT <bo1122f@gmail.com>
2024-06-14 11:45:23 +0900

    ４回目App

--- Application/src/App.tsx
+++ Application/src/App.tsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import { TextField, Button, Grid, Box } from '@mui/material';
 import CalTanabe from './tanabe.tsx';
+import CalTakazawa from './takazawa.tsx';
 
 function App() {
   const [inputA, setInputA] = useState<number>(0);
   const [inputB, setInputB] = useState<number>(0);
 
   return (
-    <>
+    <><Grid item xs={6}>
       <Box    
         display="flex"
         flexDirection="column"
@@ -31,7 +32,11 @@ function App() {
           style={{ color: "#000000", backgroundColor: "#00ffff" }} 
         />
       </Box>
+      </Grid>
+      <Grid item xs={6}>
+      <CalTakazawa inputA={inputA} inputB={inputB} />
       <CalTanabe inputA={inputA} inputB={inputB} />
+      </Grid>
     </>  
   );
 }

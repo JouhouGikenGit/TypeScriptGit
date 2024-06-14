9d0ffb7e5bafffc9ff377ac5dee4cbe5c4140e1e
NT <bo1122f@gmail.com>
2024-06-14 10:18:31 +0900

    App ３回目

--- Application/src/App.tsx
+++ Application/src/App.tsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
+import { TextField, Button, Grid, Box } from '@mui/material'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [inputA, setInputA] = useState(0);
+  const [inputB, setInputB] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    
+    <Box    
+        display="flex"
+        flexDirection="column"
+        alignItems="flex-start"
+        justifyContent="flex-start"
+        >
+      <TextField
+        type="number"
+        placeholder='数値Aを入力'
+        value={inputA || ""}
+        onChange={(e) => setInputA(Number(e.target.value))}
+        margin="normal"
+        style={{ color: "#000000", backgroundColor: "#00ffff" }} 
+      />
+      <TextField
+        type="number"
+        placeholder='数値Bを入力'
+        value={inputB || ""}
+        onChange={(e) => setInputB(Number(e.target.value))}
+        margin="normal"
+        style={{ color: "#000000", backgroundColor: "#00ffff" }} 
+      />
+    </Box>
+    
+    </>  
+  );
 }
 
 export default App

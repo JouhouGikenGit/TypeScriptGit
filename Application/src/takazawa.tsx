85cbd747a8d9a362824d5fd397f5aed5956d4bc8
NT <bo1122f@gmail.com>
2024-06-20 08:34:07 +0900

    髙澤

--- Application/src/takazawa.tsx
+++ Application/src/takazawa.tsx
@@ -8,16 +8,25 @@ type Cal = {
 
 const CalTakazawa:React.FC<Cal> = ({inputA,inputB}) => {
 const [num, setNum] = useState<number>(0);
+const [err, setErr] = useState<string>("");
 
 const hundleMinusClick = () => {
-    setNum(inputA - inputB);
+    
+    if(inputA == 0 || inputB == 0){
+        setErr("NaN");
+        setNum(0);
+        
+    }else if(inputA == 0 && inputB == 0){
+        setErr("NaN");
+    }else{
+        setNum(inputA - inputB);
+    }
 };
-
 return(
     <div>
         <Box display="flex" alignItems="center" gap={3}>
         <Button variant="contained" style={{marginTop:"20px"}} onClick={hundleMinusClick}>-</Button>
-        <TextField value={num} style={{marginTop:"20px"}} label="引き算の結果が表示されます" disabled/>
+        <TextField value={num || err} style={{marginTop:"20px"}} label="引き算の結果が表示されます" disabled/>
         </Box>
         </div>
       );
